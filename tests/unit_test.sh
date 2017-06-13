dadf1fac7ef90349fb2085f46bd3e9cef5774bd5
Arthur Del Esposte <arthurmde@gmail.com>
2017-06-13 15:12:04 -0300

    Add tests from actuator and discoverer

--- tests/unit_test.sh
+++ tests/unit_test.sh
@@ -15,4 +15,14 @@ test_from_data_collector() {
 	assertEquals 0 "$?"
 }
 
+test_from_actuator_controller() {
+	run_unit_tests actuator-controller
+	assertEquals 0 "$?"
+}
+
+test_from_resource_discoverer() {
+	run_unit_tests resource-discoverer
+	assertEquals 0 "$?"
+}
+
 load_shunit2
